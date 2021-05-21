acd5e73fc6f6970ee3ef0d4635c4f4f145b346dc
ryarasi <ragav.code@gmail.com>
2021-05-21 18:30:59 +0530

    Got login modal to work, needs some more polishing

--- src/app/pages/modals/login/login-modal.component.ts
+++ src/app/pages/modals/login/login-modal.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, Inject } from '@angular/core';
 import {
   FormBuilder,
@@ -11,9 +12,17 @@ import {
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
 import { Select, Store } from '@ngxs/store';
-import { LoginAction } from 'src/app/shared/state/auth/auth.actions';
+import {
+  LoginAction,
+  RegisterAction,
+  SendPasswordResetEmailAction,
+} from 'src/app/shared/state/auth/auth.actions';
 import { AuthState } from 'src/app/shared/state/auth/auth.state';
 import Observable from 'zen-observable';
+
+const REGISTER = 'REGISTER';
+const LOGIN = 'LOGIN';
+const FORGOT_PASSWORD = 'FORGOT_PASSWORD';
 
 @Component({
   selector: 'app-login-modal',
@@ -21,10 +30,16 @@ import Observable from 'zen-observable';
   styleUrls: ['./login-modal.component.scss'],
 })
 export class LoginModalComponent {
+  REGISTER = REGISTER;
+  LOGIN = LOGIN;
+  FORGOT_PASSWORD = FORGOT_PASSWORD;
+  showDialog: string = LOGIN;
   loginForm: FormGroup;
+  forgotPasswordForm: FormGroup;
+  registerForm: FormGroup;
   hide = true; // variable to store show/hide password toggle
-  @Select(AuthState.getIsLoggingIn)
-  isLoggingIn$: Observable<boolean>;
+  @Select(AuthState.getIsSubmittingForm)
+  isSubmittingForm$: Observable<boolean>;
 
   constructor(
     private store: Store,
@@ -33,9 +48,29 @@ export class LoginModalComponent {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private fb: FormBuilder
   ) {
+    this.setupLoginForm();
+    this.setupForgotPasswordForm();
+    this.setupRegisterForm();
+  }
+
+  setupForgotPasswordForm() {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+  setupLoginForm() {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
+    });
+  }
+
+  setupRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password1: ['', Validators.required],
+      password2: ['', Validators.required],
     });
   }
 
@@ -44,14 +79,30 @@ export class LoginModalComponent {
   }
 
   login(form: FormGroup, formDirective: FormGroupDirective) {
+    console.log('login button is clicked!');
     this.store.dispatch(new LoginAction({ form, formDirective }));
   }
 
-  register() {
-    console.log('I need to sign up');
+  register(form: FormGroup, formDirective: FormGroupDirective) {
+    console.log('register button was clicked');
+    this.store.dispatch(new RegisterAction({ form, formDirective }));
   }
 
-  forgotPassword() {
-    console.log('I forgot password');
+  sendPasswordResetEmail(form: FormGroup, formDirective: FormGroupDirective) {
+    this.store.dispatch(
+      new SendPasswordResetEmailAction({ form, formDirective })
+    );
+  }
+
+  showLogin() {
+    this.showDialog = LOGIN;
+  }
+
+  showRegister() {
+    this.showDialog = REGISTER;
+  }
+
+  showForgotPassword() {
+    this.showDialog = FORGOT_PASSWORD;
   }
 }
