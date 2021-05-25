18dd0eccd087508093e19ee9229047eb58a2e74a
ryarasi <ragav.code@gmail.com>
2021-05-25 21:06:56 +0530

    Added user queries and institution queries in the graphql query spec

--- src/app/shared/api/graphql/queries.graphql.ts
+++ src/app/shared/api/graphql/queries.graphql.ts
@@ -32,4 +32,49 @@ export const USER_QUERIES = {
       }
     }
   `,
+  GET_USERS: gql`
+    query users {
+      users {
+        username
+        id
+        name
+        title
+        bio
+        institution {
+          id
+          name
+        }
+        lastLogin
+      }
+    }
+  `,
+};
+
+export const INSTITUTION_QUERIES = {
+  GET_INSTITUTION: gql`
+    query institution($id: Int!) {
+      institution(id: $id) {
+        id
+        name
+        location
+        city
+        website
+        phone
+        logo
+        bio
+        invitecode
+      }
+    }
+  `,
+  GET_INSTITUTIONS: gql`
+    query institutions($input: any) {
+      institutions(input: $input) {
+        id
+        name
+        location
+        city
+        bio
+      }
+    }
+  `,
 };
