cdabf9a5e1905214afa04feddcf778558d36eb85
ryarasi <ragav.code@gmail.com>
2021-05-26 19:13:23 +0530

    adde the api endpoint in the environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 const api_endpoint = '';
 export const environment = {
   production: false,
+  api_endpoint,
   file_uplod_endpoint: `${api_endpoint}/upload/`,
   graphql_endpoint: `${api_endpoint}/graphql/`,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 const api_endpoint = 'http://localhost:8000';
 export const environment = {
   production: false,
+  api_endpoint,
   file_uplod_endpoint: `${api_endpoint}/upload/`,
   graphql_endpoint: `${api_endpoint}/graphql/`,
 };
