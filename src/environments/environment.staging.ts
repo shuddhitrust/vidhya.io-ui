0dad588e466caaa308aa233e91e4e32c108c218f
ragavyarasi <ragav.code@gmail.com>
2021-08-23 12:59:57 +0530

    Added a staging environment

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,20 @@
+// const api_endpoint = '';
+// const websocket_api_endpoint = '';
+// export const environment = {
+//   production: false,
+//   api_endpoint,
+//   file_uplod_endpoint: `${api_endpoint}/upload/`,
+//   graphql_endpoint: `${api_endpoint}/graphql/`,
+//   websocket_graphql_endpoint: `${websocket_api_endpoint}/graphql/`,
+// };
+
+const api_endpoint = 'https://vidhya-io-staging.herokuapp.com';
+const websocket_api_endpoint = 'wss://vidhya-io-staging.herokuapp.com';
+
+export const environment = {
+  production: false,
+  api_endpoint,
+  file_uplod_endpoint: `${api_endpoint}/upload/`,
+  graphql_endpoint: `${api_endpoint}/graphql/`,
+  websocket_graphql_endpoint: `${websocket_api_endpoint}/graphql/`,
+};
