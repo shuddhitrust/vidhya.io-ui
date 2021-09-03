e6e729d476b7053e7e6aada12890ab4e9bad3d91
ragavyarasi <ragav.code@gmail.com>
2021-09-03 11:38:17 +0530

    Validating recipients in announcment form

--- src/app/pages/forms/add-edit-announcement/add-edit-announcement.component.ts
+++ src/app/pages/forms/add-edit-announcement/add-edit-announcement.component.ts
@@ -21,6 +21,7 @@ import { AuthState } from 'src/app/shared/state/auth/auth.state';
 import { GroupState } from 'src/app/shared/state/groups/group.state';
 import { FetchGroupsAction } from 'src/app/shared/state/groups/group.actions';
 import { defaultSearchParams } from 'src/app/shared/common/constants';
+import { ShowNotificationAction } from 'src/app/shared/state/notifications/notification.actions';
 @Component({
   selector: 'app-add-edit-announcement',
   templateUrl: './add-edit-announcement.component.html',
@@ -97,10 +98,7 @@ export class AddEditAnnouncementComponent implements OnInit {
       [this.recipientsInstitution]: [
         this.announcementFormRecord?.[this.recipientsInstitution],
       ],
-      [this.groups]: [
-        announcementFormRecord?.[this.groups],
-        Validators.required,
-      ],
+      [this.groups]: [announcementFormRecord?.[this.groups]],
     });
   };
   ngOnInit(): void {
@@ -129,13 +127,36 @@ export class AddEditAnnouncementComponent implements OnInit {
     this.location.back();
   }
 
+  validateRecipients() {
+    let result = false;
+    if (this.announcementForm.get(this.recipientsGlobal).value == true) {
+      result = true;
+    }
+    if (this.announcementForm.get(this.recipientsInstitution).value == true) {
+      result = true;
+    }
+    if (this.announcementForm.get(this.groups).value.length > 0) {
+      result = true;
+    }
+    return result;
+  }
+
   submitForm(form: FormGroup, formDirective: FormGroupDirective) {
     console.log('announcement submit form value => ', form.value);
-    this.store.dispatch(
-      new CreateUpdateAnnouncementAction({
-        form,
-        formDirective,
-      })
-    );
+    if (this.validateRecipients()) {
+      this.store.dispatch(
+        new CreateUpdateAnnouncementAction({
+          form,
+          formDirective,
+        })
+      );
+    } else {
+      this.store.dispatch(
+        new ShowNotificationAction({
+          message: 'Please specify recipients',
+          action: 'error',
+        })
+      );
+    }
   }
 }
