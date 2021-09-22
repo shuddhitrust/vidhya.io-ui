d0d30dcbdf7802ca43af52e28fbd00d54ab5e7d0
ragavyarasi <ragav.code@gmail.com>
2021-09-22 18:33:37 +0530

    Minor update to member modal

--- src/app/pages/modals/member-profile/member-profile.component.ts
+++ src/app/pages/modals/member-profile/member-profile.component.ts
@@ -40,6 +40,7 @@ export class MemberProfileComponent {
     private auth: AuthorizationService
   ) {
     this.profileData = data;
+    console.log('profileData from modal', { profileData: this.data });
   }
 
   closeDialog(): void {
