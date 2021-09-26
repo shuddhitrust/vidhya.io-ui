eef85f56b9c3537e9808c3ed8f85b3e52795d65b
ragavyarasi <ragav.code@gmail.com>
2021-09-26 15:08:14 +0530

    Removed unnecessary imports

--- src/app/shared/state/exerciseSubmissions/exerciseSubmission.model.ts
+++ src/app/shared/state/exerciseSubmissions/exerciseSubmission.model.ts
@@ -6,10 +6,7 @@ import {
   FetchPolicy,
   ExerciseSubmission,
   FetchParams,
-  startingFetchParams,
 } from '../../common/models';
-import { uiroutes } from '../../common/ui-routes';
-import { emptyExerciseFormRecord } from '../exercises/exercise.model';
 
 export type GradingGroup = {
   id: number;
