b98f032d4d564974fa8946ec3f66cc09bd2f64c8
ragavyarasi <ragav.code@gmail.com>
2021-10-06 12:00:39 +0530

    Set the base for separate modules

---
+++ src/app/modules/dashboard/modules/grading/grading.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/modules/shared.module';
+
+const declarations = [];
+const imports = [SharedModule];
+
+@NgModule({
+  declarations,
+  imports,
+  exports: [...declarations, ...imports],
+})
+export class GradingModule {}
