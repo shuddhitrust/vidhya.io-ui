0f35d91c501a6698f0962b79182e1678d43539d7
ragavyarasi <ragav.code@gmail.com>
2021-10-09 21:38:04 +0530

    Removed unnecessary imports

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -4,12 +4,10 @@ import { PublicRoutingModule } from './public-routing.module';
 import { NgxsModule } from '@ngxs/store';
 import { PublicState } from './state/public/public.state';
 import { PublicComponent } from './components/public/public.component';
-import { MemberModule } from '../dashboard/modules/admin/modules/member/member.module';
 import { AuthModule } from '../auth/auth.module';
 import { ErrorPageComponent } from './components/pages/error/error.component';
 import { PublicTabsComponent } from './components/feed/public-lists.component';
 import { PublicLearnersFeedComponent } from './components/feed/learners-feed/learners-feed.component';
-import { InstitutionModule } from '../dashboard/modules/admin/modules/institution/institution.module';
 import { InstitutionProfileComponent } from './components/profiles/public-institution-profile/public-institution-profile.component';
 import { InstitutionsFeedComponent } from './components/feed/institutions-feed/institutions-feed.component';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -36,8 +34,6 @@ const declarations = [
   imports: [
     SharedModule,
     AuthModule,
-    MemberModule,
-    InstitutionModule,
     PublicRoutingModule,
     NgxsModule.forFeature([PublicState]),
   ],
