f319ea5ab5392ff3b28bc5acb18fa7e0004f4dbb
ragavyarasi <ragav.code@gmail.com>
2021-10-10 10:25:28 +0530

    Added a button mat module in master grid

--- src/app/shared/modules/master-grid/master-grid.module.ts
+++ src/app/shared/modules/master-grid/master-grid.module.ts
@@ -9,6 +9,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MasterGridComponent, PaginatorComponent],
@@ -27,6 +28,7 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    MatButtonModule,
   ],
 })
 export class MasterGridModule {}
