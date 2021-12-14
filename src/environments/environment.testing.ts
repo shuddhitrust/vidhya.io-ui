b2d2dd787c1598b16b2679c1ccc2abf0e291d5e8
ragavyarasi <ragav.code@gmail.com>
2021-12-14 23:31:31 +0530

    Added testing environment and improved main.yml of github actions

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const api_endpoint = 'https://vidhya-io-testing.herokuapp.com';
+const websocket_api_endpoint = 'wss://vidhya-io-testing.herokuapp.com';
+const cloudinary_endpoint = 'https://api.cloudinary.com/v1_1/ragav-dev';
+const cloudinary_preset = 'cljckgq2';
+
+export const environment = {
+  production: true,
+  api_endpoint,
+  file_uplod_endpoint: `${cloudinary_endpoint}/upload/`,
+  cloudinary_preset,
+  graphql_endpoint: `${api_endpoint}/graphql/`,
+  websocket_graphql_endpoint: `${websocket_api_endpoint}/graphql/`,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
