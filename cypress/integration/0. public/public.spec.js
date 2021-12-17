5cffbcf3c3a4c0ae9c54c869875d4efecb188696
ragavyarasi <ragav.code@gmail.com>
2021-12-17 10:04:38 +0530

    Optimized public spec

--- cypress/integration/0. public/public.spec.js
+++ cypress/integration/0. public/public.spec.js
@@ -1,16 +1,38 @@
 /// <reference types="cypress" />
 
 describe("Testing Public Module...", () => {
-  beforeEach(() => {
-    cy.visit("/");
+  before(() => {
+    cy.fixture("routes").as("routes");
   });
 
   it("Displays the institution and learners tabs", () => {
-    cy.get("[data-cy=parent-container]").contains("Institutions");
-    cy.get("[data-cy=parent-container]").contains("Learners");
+    cy.get("@routes").then((routes) => {
+      cy.visit(routes.HOME_ROUTE.route);
+      cy.get("[data-cy=parent-container]").contains("Institutions");
+      cy.get("[data-cy=parent-container]").contains("Learners");
+    });
   });
 
   it("has login button", () => {
-    cy.get("[data-cy=login-button]").contains("Login");
+    cy.get("@routes").then((routes) => {
+      cy.visit(routes.HOME_ROUTE.route);
+      cy.get("[data-cy=login-button]").contains("Login");
+    });
+  });
+
+  it("Displays institution profile properly", () => {
+    cy.get("@routes").then((routes) => {
+      cy.fixture("existing-records").then((existing) => {
+        cy.visit(
+          routes.INSTITUTION_PROFILE_ROUTE.route +
+            "/" +
+            existing.institution.code
+        );
+
+        cy.get("[data-cy=institution-name]").contains(
+          existing.institution.name
+        );
+      });
+    });
   });
 });
