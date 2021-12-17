e58f194d8fe5899b45e0b2e282b0241d8c4c5a1a
ragavyarasi <ragav.code@gmail.com>
2021-12-17 20:18:15 +0530

    Completed basic test suite for public module
    And updated the fixtures fully

--- cypress/integration/0. public/public.spec.js
+++ cypress/integration/0. public/public.spec.js
@@ -28,11 +28,40 @@ describe("Testing Public Module...", () => {
             "/" +
             existing.institution.code
         );
-
         cy.get("[data-cy=institution-name]").contains(
           existing.institution.name
         );
       });
+    });
+  });
+  it("Displays member profile properly", () => {
+    cy.get("@routes").then((routes) => {
+      cy.fixture("existing-records").then((existing) => {
+        cy.visit(
+          routes.MEMBER_PROFILE_ROUTE.route + "/" + existing.admin.username
+        );
+        cy.get("[data-cy=member-name]").contains(existing.admin.name);
+      });
+    });
+  });
+  it("Displays privacy page properly", () => {
+    cy.get("@routes").then((routes) => {
+      cy.visit(routes.PRIVACY_ROUTE.route);
+      cy.get("[data-cy=page-title]").contains("Privacy Policy");
+    });
+  });
+  it("Displays terms and conditions page properly", () => {
+    cy.get("@routes").then((routes) => {
+      cy.visit(routes.TERMS_CONDITIONS_ROUTE.route);
+      cy.get("[data-cy=page-title]").contains("Terms and Conditions");
+    });
+  });
+  it("Displays copyright text on home page", () => {
+    cy.get("@routes").then((routes) => {
+      cy.visit(routes.HOME_ROUTE.route);
+      cy.get(".footer").contains(
+        "Copyright © 2021 Shuddhi Vidhya Initiative, Shuddhi Trust, All Rights Reserved"
+      );
     });
   });
 });
--- cypress/integration/1. auth/auth.spec.js
+++ cypress/integration/1. auth/auth.spec.js
@@ -5,11 +5,6 @@ describe("Testing Public Module...", () => {
     cy.visit("/");
   });
 
-  it("Displays the institution and learners tabs", () => {
-    cy.get("[data-cy=parent-container]").contains("Institutions");
-    cy.get("[data-cy=parent-container]").contains("Learners");
-  });
-
   it("has login button", () => {
     cy.get("[data-cy=login-button]").contains("Login");
   });
