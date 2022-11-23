a8e58b913324b0ccbc1e8baec7ae71e39c73cf6d
Ragav Yarasi <ragav.code@gmail.com>
2022-11-23 19:13:47 +0530

    Updated the API for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,8 @@
 //   websocket_graphql_endpoint: `${websocket_api_endpoint}/graphql/`,
 // };
 
-const api_endpoint = 'https://vidhya-io.herokuapp.com';
-const websocket_api_endpoint = 'wss://vidhya-io.herokuapp.com';
+const api_endpoint = 'https://vidhya-io-api.onrender.com';
+const websocket_api_endpoint = 'wss://vidhya-io-api.onrender.com';
 const cloudinary_endpoint = 'https://api.cloudinary.com/v1_1/svidhya';
 const cloudinary_preset = 'l4vdzicq';
 
